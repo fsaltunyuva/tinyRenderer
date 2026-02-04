c2e6a796fad302fdf61988e5f72051b99cd9a31f
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2026-02-04 18:26:10 +0300

    bugged wireframe

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
 #include "tgaimage.h"
 #include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+struct vec3{
+  float x, y, z;
+};
+
+struct ivec3{
+  int x, y, z;
+};
 
 // constexpr does computation at compile time rather than run time
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
@@ -163,7 +178,7 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
         std::swap(ax, bx);
         std::swap(ay, by);
     }
-    
+
     int y = ay;
     int ierror = 0;
     for (int x=ax; x<=bx; x++) {
@@ -177,14 +192,22 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
     }
 }
 
+pair<int,int> project(const vec3& v, int width, int height) {
+    int x = static_cast<int>((v.x + 1.f) * width  * 0.5f);
+    int y = static_cast<int>((v.y + 1.f) * height * 0.5f);
+    return {x, y};
+}
+
+void readObjFile(string fileName, vector<vec3> &vertices, vector<ivec3> &faces);
+
 int main(int argc, char** argv) {
-    constexpr int width  = 64;
-    constexpr int height = 64;
+    constexpr int width  = 800;
+    constexpr int height = 800;
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    int ax =  7, ay =  3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
+    // int ax =  7, ay =  3;
+    // int bx = 12, by = 37;
+    // int cx = 62, cy = 53;
 
     // Attempt 1
     // line1(ax, ay, bx, by, framebuffer, blue);
@@ -228,15 +251,88 @@ int main(int argc, char** argv) {
     // line3_optimization2(cx, cy, ax, ay, framebuffer, yellow);
     // line3_optimization2(ax, ay, cx, cy, framebuffer, red);
      
-    line(ax, ay, bx, by, framebuffer, blue);
-    line(cx, cy, bx, by, framebuffer, green);
-    line(cx, cy, ax, ay, framebuffer, yellow);
-    line(ax, ay, cx, cy, framebuffer, red);
+    // line(ax, ay, bx, by, framebuffer, blue);
+    // line(cx, cy, bx, by, framebuffer, green);
+    // line(cx, cy, ax, ay, framebuffer, yellow);
+    // line(ax, ay, cx, cy, framebuffer, red);
 
-    framebuffer.set(ax, ay, white);
-    framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
+    // framebuffer.set(ax, ay, white);
+    // framebuffer.set(bx, by, white);
+    // framebuffer.set(cx, cy, white);
+
+    // framebuffer.write_tga_file("framebuffer.tga");
+
+    vector<vec3> vertices;
+    vector<ivec3> faces;
+    readObjFile("../models/diablo3_pose.obj", vertices, faces);
+
+    for (int i = 0; i < faces.size(); i++) {
+        vec3 v0 = vertices[faces[i].x - 1];
+        vec3 v1 = vertices[faces[i].y - 1];
+        vec3 v2 = vertices[faces[i].z - 1];
+
+        auto [x0, y0] = project(v0, width, height);
+        auto [x1, y1] = project(v1, width, height);
+        auto [x2, y2] = project(v2, width, height);
+
+        line(x0, y0, x1, y1, framebuffer, red);
+        line(x1, y1, x2, y2, framebuffer, red);
+        line(x2, y2, x0, y0, framebuffer, red);
+    }
 
     framebuffer.write_tga_file("framebuffer.tga");
+
     return 0;
+}
+
+void split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+}
+
+vector<string> split(const string &s, char delim) {
+    vector<string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
+void readObjFile(string fileName, vector<vec3> &vertices, vector<ivec3> &faces){
+    ifstream ReadObjFile(fileName);
+    string input;
+
+    while (getline(ReadObjFile, input)) {
+        if (input[0] == 'v'){
+            vector<string> strings;
+            strings = split(input, ' ');
+            if (strings[0] == "v"){
+                vec3 tempvec3;
+                tempvec3.x = stof(strings[1]);
+                tempvec3.y = stof(strings[2]);
+                tempvec3.z = stof(strings[3]);
+                vertices.push_back(tempvec3);
+            }
+        }
+
+        if (input[0] == 'f'){
+            vector<string> strings;
+            strings = split(input, ' ');
+            ivec3 tempvec3;
+
+            for (int i = 1; i < 4; i++){
+                vector<string> values;
+                values = split(strings[i], '/');
+
+                if (i == 0) tempvec3.x = stoi(values[0]);
+                if (i == 1) tempvec3.y = stoi(values[0]);
+                if (i == 2) tempvec3.z = stoi(values[0]);
+            }
+
+            faces.push_back(tempvec3);
+        }
+    }
+
+    ReadObjFile.close();
 }
